ff39f44b25ba4e59f395d637be0552dd7c27f37b
MathisBuissonAdrar <onsibuno@gmail.com>
2024-07-08 11:53:30 +0200

    fetch par ID, affichage 1 par 1

--- script.js
+++ script.js
@@ -1,46 +1,36 @@
-let listePokemon = document.querySelector;
+let total = 151;
+let text = "";
 
 const contactApi = async () => {
+    for (let offset = 1; offset <= total; offset++) {
+        const data = await fetch("https://pokeapi.co/api/v2/pokemon/"+offset);
+        console.log(data);
+        const dataTransformed = await data.json();
+        console.log(dataTransformed);
 
-    const data = await fetch("https://pokeapi.co/api/v2/pokemon/?limit=151&offset=0");
-    // console.log(data);
-    const dataTransformed = await data.json();
-    // console.log(dataTransformed);
+        let nomType = "";
+        let lienDescription = "";
+        let pkmnDescription = "";
 
-    listePokemon.innerText = dataTransformed;
+        // for (const element of dataTransformed.results) {
+            nomType = "";
+            console.log(dataTransformed.name);
 
-    let text = "";
-    let lienImage = "";
-    let nomType = "";
-    let lienDescription ="";
-    let pkmnDescription ="";
+            lienDescription = dataTransformed.species.url;
+            const dataPkmn = await fetch(lienDescription);
+            const dataPkmnTransformed = await dataPkmn.json();
+            // console.log(dataPkmnTransformed);
+            pkmnDescription = dataPkmnTransformed.flavor_text_entries[3].flavor_text;
 
-    for (const element of dataTransformed.results){
-        console.log(element.name);
-
-        lienImage = element.url ;
-        const dataImage = await fetch(lienImage);
-        // console.log(dataImage);
-        const dataImageTransformed = await dataImage.json();
-        // console.log(dataImageTransformed);
-        // console.log(dataImageTransformed.sprites.back_default);
-
-        lienDescription = dataImageTransformed.species.url;
-        const dataPkmn = await fetch(lienDescription);
-        const dataPkmnTransformed = await dataPkmn.json();
-        // console.log(dataPkmnTransformed);
-        pkmnDescription = dataPkmnTransformed.flavor_text_entries[3].flavor_text ;
-
-        for (const pkmnType of dataImageTransformed.types) {
-            nomType += '<img src="./images/' + pkmnType.type.name + '.png"> ' ;
-        }
-        
-        text += '<div class="pokemon"><p><img src="./images/3869.png"> ' + element.name +'<br>'+ nomType +'</p>' + '<img src=' + dataImageTransformed.sprites.front_default + ' class="sprite"><p class="description">' + pkmnDescription +  '</p></div>';
-        nomType = "";
+            for (const pkmnType of dataTransformed.types) {
+                nomType += '<img src="./images/' + pkmnType.type.name + '.png"> ';
+            }
+            text = '<div class="pokemon"><p><img src="./images/3869.png"> ' + dataTransformed.name + '<br>' + nomType + '</p>' + '<img src=' + dataTransformed.sprites.front_default + ' class="sprite"><p class="description">' + pkmnDescription + '</p></div>';
+        // }
+        document.getElementById("liste").innerHTML += text;
     }
-    document.getElementById("liste").innerHTML = text;
 }
 
 
+    contactApi();
 
-contactApi();
