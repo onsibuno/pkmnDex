714f13c001fe8df9eb3a51ee5ca5d9ce477cfb26
MathisBuissonAdrar <onsibuno@gmail.com>
2024-07-09 11:54:30 +0200

    ajouts de variables pour futures utilisations, CSS footer

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 let total = 151;
-let text = "";
+let boxPKMN = "";
+let premierPKMN = 1;
 
 const contactApi = async () => {
-    for (let offset = 1; offset <= total; offset++) {
+    for (let offset = premierPKMN; offset <= total; offset++) {
         const data = await fetch("https://pokeapi.co/api/v2/pokemon/"+offset);
         console.log(data);
         const dataTransformed = await data.json();
@@ -12,22 +13,22 @@ const contactApi = async () => {
         let lienDescription = "";
         let pkmnDescription = "";
 
-        // for (const element of dataTransformed.results) {
-            nomType = "";
-            console.log(dataTransformed.name);
+        nomType = "";
+        console.log(dataTransformed.name);
 
-            lienDescription = dataTransformed.species.url;
-            const dataPkmn = await fetch(lienDescription);
-            const dataPkmnTransformed = await dataPkmn.json();
-            // console.log(dataPkmnTransformed);
-            pkmnDescription = dataPkmnTransformed.flavor_text_entries[3].flavor_text;
+        lienDescription = dataTransformed.species.url;
+        const dataPkmn = await fetch(lienDescription);
+        const dataPkmnTransformed = await dataPkmn.json();
+        // console.log(dataPkmnTransformed);
+        pkmnDescription = dataPkmnTransformed.flavor_text_entries[8].flavor_text;
 
-            for (const pkmnType of dataTransformed.types) {
-                nomType += '<img src="./images/' + pkmnType.type.name + '.png"> ';
-            }
-            text = '<div class="pokemon"><p><img src="./images/3869.png"> ' + dataTransformed.name + '<br>' + nomType + '</p>' + '<img src=' + dataTransformed.sprites.front_default + ' class="sprite"><p class="description">' + pkmnDescription + '</p></div>';
-        // }
-        document.getElementById("liste").innerHTML += text;
+        for (const pkmnType of dataTransformed.types) {
+            nomType += '<img src="./images/' + pkmnType.type.name + '.png"> ';
+        }
+
+        boxPKMN = '<div class="pokemon"><p><img src="./images/3869.png"> ' + dataTransformed.name + '<br>' + nomType + '</p>' + '<img src=' + dataTransformed.sprites.front_default + ' class="sprite"><p class="description">' + pkmnDescription + '</p></div>';
+        document.getElementById("liste").innerHTML += boxPKMN;   
+        // console.log(dataTransformed.sprites.versions["generation-iv"]["heartgold-soulsilver"].front_default)
     }
 }
 
